75a51912d3781072bc7e58c0b919f92ee60bcb1f
snikiel <s.nikiel@stud.elka.pw.edu.pl>
2021-05-27 12:07:33 +0200

    Zmiana progowania HSV na HLS

--- BlocksDetector.cpp
+++ BlocksDetector.cpp
@@ -9,40 +9,49 @@
 #include "FileSystemHelper.h"
 #include "Converter.h"
 #include "Filter.h"
+#include <stack>
 
 
 void showImage(cv::Mat image, std::string title)
 {
-    cv::resize(image, image, cv::Size(image.cols, image.rows));
+    int cols = image.cols;
+    int rows = image.rows;
+    while (rows > 800)
+    {
+        rows /= 2;
+        cols /= 2;
+    }
+    cv::resize(image, image, cv::Size(cols, rows));
     cv::imshow(title, image); 
 }
 
-cv::Mat hsvThreshold(cv::Mat source)
+static cv::Vec3b randomColor()
 {
-    cv::Mat_<cv::Vec3b> outImage = source.clone();
+    cv::RNG rng(12345);
+    return cv::Vec3b((uchar)rng.uniform(0, 255), (uchar)rng.uniform(0, 255), (uchar)rng.uniform(0, 255));
+}
+
+cv::Mat threshold(cv::Mat source)
+{
+    cv::Mat_<uchar> outImage = cv::Mat::zeros(source.size(), CV_8U);
+
     cv::Mat_<cv::Vec3b> image;
-    cv::cvtColor(source, image, cv::COLOR_BGR2HSV);
+    cv::cvtColor(source, image, cv::COLOR_BGR2HLS);
 
-    // H - 0-179
-    // S - 0-255
-
-    int lowH = 0; //0
-    int highH = 15; //15 -- hist 25/27
-    int lowS = 106;//95 --hist 55 #new 107
-    int highS = 230;//230
-    int highV = 210;//190
+    int lowH = 3; //0 -- HSV //3 -- HLS
+    int highH = 16; //15 -- HSV //16 -- HLS
+    int lowS = 65; //106 -- HSV //65 -- HLS
+    int highS = 190; //230 -- HSV //170 -- HLS
+    int lowL = 0; //0 -- HLS
+    int highL = 145; //145 -- HLS
 
     for (int x = 0; x < image.cols; x++)
     {
         for (int y = 0; y < image.rows; y++)
         {
-            if (image(y,x)[1] >= lowS && image(y, x)[1] <= highS && image(y, x)[0] >= lowH && image(y, x)[0] <= highH /*&& image(y,x)[2] < highV*/)
+            if (image(y,x)[2] >= lowS && image(y, x)[2] <= highS && image(y, x)[0] >= lowH && image(y, x)[0] <= highH && image(y, x)[1] >= lowL && image(y, x)[1] <= highL)
             {
-                outImage(y, x)[0] = outImage(y, x)[1] = outImage(y, x)[2] = 255;
-            }
-            else
-            {
-                outImage(y, x)[0] = outImage(y, x)[1] = outImage(y,x)[2] = 0;
+                outImage(y, x) = 255;
             }
         }
     }
@@ -50,8 +59,38 @@ cv::Mat hsvThreshold(cv::Mat source)
     return outImage;
 }
 
+void floodFill(cv::Mat_<cv::Vec3b>& image, cv::Mat_<uchar>& mask, cv::Vec3b color, cv::Point2i seed, int threshold, bool eightConnected = true)
+{
+    uchar done = 0;
+    std::stack<cv::Point2i> stack = std::stack<cv::Point2i>();
+
+    if (mask(seed) == done)
+    {
+        return;
+    }
+    else
+    {
+        stack.push(seed);
+    }    
+
+    while (!stack.empty())
+    {
+        cv::Point2i currentPixel = stack.top();
+        stack.pop();
+
+        image(currentPixel) = color;
+        mask(currentPixel) = done;
+
+        if (true)
+        {
+            
+        }
 
 
+    }
+
+
+}
 
 int main(int, char* []) 
 {
@@ -64,12 +103,17 @@ int main(int, char* [])
     {
         cv::Mat image = images[i];
 
-        image = Filter::rankFilter(image, 3, 2);
+        image = Filter::rankFilter(image, 3, 5);
         showImage(image, "rank" + std::to_string(i));
         cv::imwrite("rank" + std::to_string(i) + ".jpg", image);
 
-        image = hsvThreshold(image);
-        showImage(image, std::to_string(i));
+        image = threshold(image);
+
+        cv::dilate(image, image, cv::Mat());
+        cv::dilate(image, image, cv::Mat());
+        cv::erode(image, image, cv::Mat());
+
+        showImage(image, "out" + std::to_string(i));
         cv::imwrite("out" + std::to_string(i) + ".jpg", image);
     }
     
